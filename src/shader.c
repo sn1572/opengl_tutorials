9a78cbdd99838519d36e72baa8ac0bbe5d9829ab
Mark Bolding <markbolding@LAPTOP-F948Q6C6.attlocal.net>
2021-12-14 09:05:42 -0500

    retabbed shader.c

--- src/shader.c
+++ src/shader.c
@@ -4,37 +4,37 @@
 
 void readFile(const char * fname, char ** buffer)
 {
-	// buffer does not need to be pre-allocated
-	FILE * fp;
-	long size;
+    // buffer does not need to be pre-allocated
+    FILE * fp;
+    long size;
 
-	fp = fopen (fname, "rb");
-	if(!fp){
-		perror("readFile");
-		exit(1);
-	}
-	fseek(fp , 0L , SEEK_END);
-	size = ftell(fp);
-	rewind(fp);
+    fp = fopen (fname, "rb");
+    if(!fp){
+        perror("readFile");
+        exit(1);
+    }
+    fseek(fp , 0L , SEEK_END);
+    size = ftell(fp);
+    rewind(fp);
 
-	/* allocate memory for entire content */
-	*buffer = calloc(size+1, sizeof(char));
-	if(!buffer){
-		fclose(fp);
+    /* allocate memory for entire content */
+    *buffer = calloc(size+1, sizeof(char));
+    if(!buffer){
+        fclose(fp);
         err_print("Failed to allocate memory\n");
-		exit(1);
-	}
+        exit(1);
+    }
 
-	/* copy the file into the buffer */
-	if(1 != fread(*buffer, size, 1, fp)){
-		fclose(fp);
-		free(*buffer);
-		fputs("File read failure", stderr);
-		exit(1);
-	}
-	// NUL terminate the buffer
-	(*buffer)[size] = '\0';
-	fclose(fp);
+    /* copy the file into the buffer */
+    if(1 != fread(*buffer, size, 1, fp)){
+        fclose(fp);
+        free(*buffer);
+        fputs("File read failure", stderr);
+        exit(1);
+    }
+    // NUL terminate the buffer
+    (*buffer)[size] = '\0';
+    fclose(fp);
 }
 
 
@@ -45,31 +45,31 @@ shader_err_t load(struct Shader * self, char * vertexPath, char * fragmentPath)
     unsigned int vertex, fragment;
     shader_err_t result = SHADER_NO_ERR;
 
-	readFile(vertexPath, &vertexSource);
-	if (!(vertexSource)){
-		err_print("Vertex shader file read failure\n");
+    readFile(vertexPath, &vertexSource);
+    if (!(vertexSource)){
+        err_print("Vertex shader file read failure\n");
         return SHADER_NULL_PTR;
-	}
-	readFile(fragmentPath, &fragmentSource);
-	if (!(fragmentSource)){
-		err_print("Fragment shader file read failure\n");
+    }
+    readFile(fragmentPath, &fragmentSource);
+    if (!(fragmentSource)){
+        err_print("Fragment shader file read failure\n");
         result = SHADER_NULL_PTR;
         goto free_1;
-	}
+    }
 
     // vertex shader
     vertex = glCreateShader(GL_VERTEX_SHADER);
-	if (glGetError() != GL_NO_ERROR){
-		err_print("shader creation failed\n");
+    if (glGetError() != GL_NO_ERROR){
+        err_print("shader creation failed\n");
         result = SHADER_GL_ERR;
         goto free_2;
-	}
+    }
     glShaderSource(vertex, 1, (const char **)(&vertexSource), NULL);
-	if (glGetError() != GL_NO_ERROR){
-		err_print("shader source assignment failed\n");
+    if (glGetError() != GL_NO_ERROR){
+        err_print("shader source assignment failed\n");
         result = SHADER_GL_ERR;
         goto shader_1;
-	}
+    }
     glCompileShader(vertex);
     if (checkCompileErrors(vertex, "VERTEX") != SHADER_NO_ERR){
         err_print("Vertex shader compile failures.\n");
@@ -86,11 +86,11 @@ shader_err_t load(struct Shader * self, char * vertexPath, char * fragmentPath)
         goto shader_1;
     }
     glShaderSource(fragment, 1, (const char **)(&fragmentSource), NULL);
-	if (glGetError() != GL_NO_ERROR){
-		err_print("shader source assignment failed\n");
+    if (glGetError() != GL_NO_ERROR){
+        err_print("shader source assignment failed\n");
         result = SHADER_GL_ERR;
         goto shader_2;
-	}
+    }
     glCompileShader(fragment);
     if (checkCompileErrors(vertex, "VERTEX") != SHADER_NO_ERR){
         err_print("Vertex shader compile failures.\n");
@@ -116,7 +116,7 @@ shader_err_t load(struct Shader * self, char * vertexPath, char * fragmentPath)
         goto shader_2;
     }
     /* The penultimate assignment. If we got here, we succeeded. */
-	self->ID = ID;
+    self->ID = ID;
 
     /* Cleanup */
     shader_2:
@@ -124,9 +124,9 @@ shader_err_t load(struct Shader * self, char * vertexPath, char * fragmentPath)
     shader_1:
         glDeleteShader(vertex);
     free_2:
-	    free(fragmentSource);
+        free(fragmentSource);
     free_1:
-	    free(vertexSource);
+        free(vertexSource);
     return result;
 }
 
@@ -139,52 +139,52 @@ void use(struct Shader * self)
 
 void setBool(struct Shader * self, const char * name, int value)
 {
-	glUniform1i(glGetUniformLocation(self->ID, name), (int)value); 
+    glUniform1i(glGetUniformLocation(self->ID, name), (int)value); 
 }
 
 
 void setInt(struct Shader * self, const char * name, int value)
 {
-	glUniform1i(glGetUniformLocation((*self).ID, name), value); 
+    glUniform1i(glGetUniformLocation((*self).ID, name), value); 
 }
 
 
 void setFloat(struct Shader * self, const char * name, float value)
 {
-	glUniform1f(glGetUniformLocation((*self).ID, name), value); 
+    glUniform1f(glGetUniformLocation((*self).ID, name), value); 
 }
 
 
 void setVec3(struct Shader * self, const char * name, float x, float y,
     float z)
 {
-	//float vec[] = {x,y,z};
-	glUniform3f(glGetUniformLocation((*self).ID, name), x, y, z); 
+    //float vec[] = {x,y,z};
+    glUniform3f(glGetUniformLocation((*self).ID, name), x, y, z); 
 }
 
 
 struct Shader * shaderInit()
 {
-	struct Shader * out = NULL;
-	out = calloc(1, sizeof(struct Shader));
-	if (out == NULL){
-		fprintf(stderr, "calloc failure in %s, file %s\n", __func__, __FILE__);
-		exit(1);
-	}
-	(*out).load = load;
-	(*out).use = use;
-	(*out).setBool = setBool;
-	(*out).setInt = setInt;
-	(*out).setFloat = setFloat;
-	(*out).setVec3 = setVec3;
-	(*out).self = out;
-	return out;
+    struct Shader * out = NULL;
+    out = calloc(1, sizeof(struct Shader));
+    if (out == NULL){
+        fprintf(stderr, "calloc failure in %s, file %s\n", __func__, __FILE__);
+        exit(1);
+    }
+    (*out).load = load;
+    (*out).use = use;
+    (*out).setBool = setBool;
+    (*out).setInt = setInt;
+    (*out).setFloat = setFloat;
+    (*out).setVec3 = setVec3;
+    (*out).self = out;
+    return out;
 }
 
 
 shader_err_t checkCompileErrors(unsigned int shader, char * type)
 {
-	int success;
+    int success;
     char infoLog[1024];
     if (type != "PROGRAM"){
         glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
@@ -210,55 +210,55 @@ shader_err_t checkCompileErrors(unsigned int shader, char * type)
 
 void shader_introspection(struct Shader * shaders)
 {
-	// Stolen from Stack Overflow
-	unsigned int active_program = shaders->ID;
+    // Stolen from Stack Overflow
+    unsigned int active_program = shaders->ID;
 
-	if (!glIsProgram(active_program)) {
-	    printf("Active program is not valid.\n");
-	    exit(1);
-	}
+    if (!glIsProgram(active_program)) {
+        printf("Active program is not valid.\n");
+        exit(1);
+    }
 
-	GLint program_valid = 0;
-	glValidateProgram(active_program);
-	glGetProgramiv(active_program, GL_VALIDATE_STATUS, &program_valid);
-	if (GL_TRUE != program_valid) {
-	    printf("Program validation failed.\n");
-	    exit(1);
-	}
+    GLint program_valid = 0;
+    glValidateProgram(active_program);
+    glGetProgramiv(active_program, GL_VALIDATE_STATUS, &program_valid);
+    if (GL_TRUE != program_valid) {
+        printf("Program validation failed.\n");
+        exit(1);
+    }
 
-	GLint current_program = 0;
-	glGetIntegerv(GL_CURRENT_PROGRAM, &current_program);
-	if (0 == current_program) {
-	    printf("Error, no current program is set.\n");
-	    exit(1);
-	} else if (current_program != active_program) {
-	    printf("Error, current program doesn't match active_program!\n");
-	}
+    GLint current_program = 0;
+    glGetIntegerv(GL_CURRENT_PROGRAM, &current_program);
+    if (0 == current_program) {
+        printf("Error, no current program is set.\n");
+        exit(1);
+    } else if (current_program != active_program) {
+        printf("Error, current program doesn't match active_program!\n");
+    }
 
-	GLint num_active_uniforms = 0;
-	glGetProgramiv(active_program, GL_ACTIVE_UNIFORMS, &num_active_uniforms);
-	if (0 == num_active_uniforms) {
-	    printf("There are 0 uniforms active in program %d.\n", active_program);
-	    exit(1);
-	} else {
-	    printf("There are %d uniform(s) active in program %d.\n",
+    GLint num_active_uniforms = 0;
+    glGetProgramiv(active_program, GL_ACTIVE_UNIFORMS, &num_active_uniforms);
+    if (0 == num_active_uniforms) {
+        printf("There are 0 uniforms active in program %d.\n", active_program);
+        exit(1);
+    } else {
+        printf("There are %d uniform(s) active in program %d.\n",
                num_active_uniforms, active_program);
-	}
+    }
 
-	GLint i;
-	GLint count;
+    GLint i;
+    GLint count;
 
-	GLint size; // size of the variable
-	GLenum type; // type of the variable (float, vec3 or mat4, etc)
+    GLint size; // size of the variable
+    GLenum type; // type of the variable (float, vec3 or mat4, etc)
 
-	const GLsizei bufSize = 16; // maximum name length
-	GLchar name[bufSize]; // variable name in GLSL
-	GLsizei length; // name length
+    const GLsizei bufSize = 16; // maximum name length
+    GLchar name[bufSize]; // variable name in GLSL
+    GLsizei length; // name length
 
-	for (i = 0; i < num_active_uniforms; i++)
-	{
-	    glGetActiveUniform(active_program, (GLuint)i, bufSize, &length,
+    for (i = 0; i < num_active_uniforms; i++)
+    {
+        glGetActiveUniform(active_program, (GLuint)i, bufSize, &length,
                            &size, &type, name);
-	    printf("Uniform #%d Type: %u Name: %s\n", i, type, name);
-	}
+        printf("Uniform #%d Type: %u Name: %s\n", i, type, name);
+    }
 }

ce08bf8a49ef0f3238c83e2549a748aa64ac5bad
Mark Bolding <markbolding@LAPTOP-F948Q6C6.attlocal.net>
2021-12-21 12:29:10 -0500

    Outsmarted myself with the spotlight directions and acos usage.

--- src/lighting/light_casters/spotlight.c
+++ src/lighting/light_casters/spotlight.c
@@ -259,8 +259,8 @@ int main(){
     setVec3(cube_shaders, "light.diffuse", light_diffuse);
     setVec3(cube_shaders, "light.specular", light_specular);
     setVec3(cube_shaders, "light.direction", light_direction);
-    setFloat(cube_shaders, "light.theta_taper_start", 0.6f);
-    setFloat(cube_shaders, "light.theta_min", 0.5f);
+    setFloat(cube_shaders, "light.theta_taper_start", 0.7f);
+    setFloat(cube_shaders, "light.theta_min", 0.6f);
 
     light_shaders->use(light_shaders);
     light_shaders->setVec3(light_shaders, "light.ambient", light_specular);

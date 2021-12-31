d2a56067259f72900a7639fb993902a521f3069b
Mark <sn1572@gmail.com>
2021-12-31 16:40:55 -0500

    Added shadow-mapping section

---
+++ src/advanced_lighting/shadow_mapping/main.c
@@ -0,0 +1,183 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <shader.h>
+#include <stdbool.h>
+#include <math.h>
+#include <linmath.h>
+#include <linmath_extension.h>
+#include <camera.h>
+#include <shader.h>
+#include <model.h>
+
+
+#define SUCCESS 0;
+#define FAILURE 1;
+
+
+static char model_frag_source[] = "shaders/vert_normal.frag";
+static char model_vert_source[] = "shaders/model2.vert";
+static char light_frag_source[] = "shaders/light_frag";
+static char light_vert_source[] = "shaders/light_vert";
+static int WIDTH = 1920;
+static int HEIGHT = 1080;
+
+
+void print_array(float * array, int rows, int cols){
+    for (int i=0; i<rows; i++){
+        for (int j=0; j<cols; j++){
+            printf("%4.2f ", array[i*cols+j]);
+        }
+        printf("\n");
+    }
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
+
+int main(){
+    int status = SUCCESS;
+    int numFrames = 0;
+    mat4x4 model_matrix;
+    mat4x4 normal_matrix;
+    float past, time;
+    char model_path[] = "../../model_loading/model/models/backpack/"
+                        "backpack.obj";
+
+    /* glfw init and context creation */
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpengl", NULL,
+                                          NULL);
+    if (window == NULL){
+        fprintf(stderr, "Failed to create a GLFW window.\n");
+        status = FAILURE;
+        goto cleanup_glfw;
+    }
+    glfwMakeContextCurrent(window);
+    /* user input callbacks */
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, glfwCompatMouseMovementCallback);
+    glfwSetScrollCallback(window, glfwCompatMouseScrollCallback);
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    //initialize GLAD loader
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        fprintf(stderr, "Failed to initialize GLAD\n");
+        status = FAILURE;
+        goto cleanup_glfw;
+    }
+
+    // set default window size
+    glViewport(0, 0, WIDTH, HEIGHT);
+
+    /* model loading
+     * Eventually this will need to have its own thread to prevent
+     * application non-responsiveness. To do that we probably need to
+     * pull out the opengl calls.
+     */
+
+    Model backpack;
+    backpack.file_path = model_path;
+    backpack.meshes = NULL;
+    backpack.directory = NULL;
+    backpack.num_meshes = 0;
+    backpack.loaded_textures = NULL;
+    if (load_model(&backpack)){
+        fprintf(stderr, "%s %d: Failed to load backpack model.\n", __FILE__,
+                __LINE__);
+        goto end;
+    }
+    setup_model(&backpack);
+
+    Texture_Node * node;
+    int num_textures = 0;
+    for (node = backpack.loaded_textures; node; node = node->next){
+        num_textures += 1;
+    }
+    printf("Backpack texture count: %i\n", num_textures);
+
+    /* Shader init */
+    struct Shader * model_shader = shaderInit();
+    if (load(model_shader, model_vert_source,
+             model_frag_source) != SHADER_NO_ERR){
+        fprintf(stderr, "model shader compilation error\n");
+        goto cleanup_gl;
+    }
+
+    // Init the camera `object` and hook its methods into the callbacks
+    struct Camera * cam;
+    cam = cameraInit(WIDTH, HEIGHT);
+    cam->movementSpeed = 5.f;
+    setActiveCamera(cam);
+    setActiveCameraPosition(0, 0, 3);
+
+    /* callback assignment former location */
+
+    /* main loop */
+    past = (float)glfwGetTime();
+    /* Turn on depth testing before drawing anything */
+    glEnable(GL_DEPTH_TEST);
+
+    while (!glfwWindowShouldClose(window)){
+        numFrames += 1;
+        time = (float)glfwGetTime();
+
+        glClearColor(0.f, 0.f, 0.f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        // Can this be moved outside the main loop?
+        glfwCompatKeyboardCallback(window);
+
+        use(model_shader);
+        setViewMatrix(cam, model_shader, "view");
+        setProjectionMatrix(cam, model_shader, "projection");
+        mat4x4_identity(model_matrix);
+        mat4x4_identity(normal_matrix);
+        sendMatrixToShader(model_matrix, "model_matrix", model_shader);
+        sendMatrixToShader(normal_matrix, "normal_matrix", model_shader);
+
+        /* apply point light effects */
+        vec3 point_ambient = {0.4f, 0.4f, 0.4f};
+        vec3 point_diffuse = {1.f, 1.f, 1.f};
+        vec3 point_specular = {3.f, 3.f, 3.f};
+        vec4 light_initial_position = {4.f, 0.f, 0.f, 0.f};
+        vec4 light_position_4;
+        vec3 light_position;
+        mat4x4 R;
+        mat4x4_identity(R);
+        float angle = time*50*M_PI/180;
+        mat4x4_rotate(R, R, 0.f, 1.f, 0.f, angle);
+        mat4x4_mul_vec4(light_position_4, R, light_initial_position);
+        light_position[0] = light_position_4[0];
+        light_position[1] = light_position_4[1];
+        light_position[2] = light_position_4[2];
+        setVec3(model_shader, "point_light.position", light_position);
+        setVec3(model_shader, "point_light.ambient", point_ambient);
+        setVec3(model_shader, "point_light.specular", point_specular);
+        setVec3(model_shader, "point_light.diffuse", point_diffuse);
+        setFloat(model_shader, "point_light.constant", 1.f);
+        setFloat(model_shader, "point_light.linear", 0.07f);
+        setFloat(model_shader, "point_light.quadratic", 0.017f);
+        setFloat(model_shader, "material.shininess", 4.f);
+
+        draw_model(model_shader, backpack);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    time = (float)glfwGetTime();
+    printf("Rendered %i frames in %1.10f seconds amounting to %f FPS.\n",
+           numFrames, time, numFrames/time);
+
+    cleanup_gl:
+        free_model(&backpack);
+    cleanup_glfw:
+        glfwTerminate();
+    end:
+        return status;
+}
---
+++ src/advanced_lighting/shadow_mapping/shaders/model.vert
@@ -0,0 +1,49 @@
+#version 400 core
+layout (location=0) in vec3 in_position;
+layout (location=1) in vec3 in_normal;
+layout (location=2) in vec2 in_texture_coordinates;
+layout (location=3) in vec3 in_tangent;
+layout (location=4) in vec3 in_bitangent;
+
+out vec3 fragment_position;
+out vec3 normal;
+out vec2 texture_coordinates; 
+out vec3 view_direction;
+out vec3 light_direction;
+out mat3 tbn_matrix;
+
+struct Light {
+    vec3 position;           //not for directional
+    vec3 direction;
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+    float theta_min;         //spotlight
+    float theta_taper_start; //spotlight
+    float constant;          //point light
+    float linear;            //point light
+    float quadratic;         //point light
+};
+
+uniform mat4 projection;
+uniform mat4 view;
+uniform mat4 model_matrix;
+uniform mat4 normal_matrix;
+uniform vec3 camera_position;
+uniform Light point_light;
+
+
+void main(){
+    fragment_position = vec3(model_matrix * vec4(in_position, 1.0));
+	texture_coordinates = in_texture_coordinates;
+	gl_Position = projection * view * model_matrix * vec4(in_position, 1.0);
+    vec3 tangent = normalize(vec3(normal_matrix * vec4(in_tangent, 0.0)));
+    vec3 bitangent = normalize(vec3(normal_matrix * vec4(in_bitangent, 0.0)));
+    normal = normalize(vec3(normal_matrix * vec4(in_normal, 0.0)));
+    tbn_matrix = mat3(tangent, bitangent, normal);
+    mat3 tbn_transpose = transpose(tbn_matrix);
+    light_direction = tbn_transpose * normalize(point_light.position \
+                                                - fragment_position);
+    view_direction = tbn_transpose * normalize(camera_position \
+                                               - fragment_position);
+}
